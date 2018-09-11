2d3c2630cbb5f95fb0950ba154cd3a2cddfd93c7
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-09-11 12:30:08 -0400

    Set up of basic functions and testing.

--- c_calculator/main.c
+++ c_calculator/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double multiply(double A, double B){
+
+return A*B;
+}
+
+double add(double A, double B){
+
+return A + B;
+}
+
+double divide(double A, double B){
+
+return A / B;
+}
+
+double subtract(double A, double B){
+
+return A - B;
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    double testA = 2;
+    double testB = 4;
+    printf("Enter first number: ");
+
+    printf("\n Add: %f", add(testA, testB));
+    printf("\n Subtract: %f", subtract(testB, testA));
+    printf("\n Multiply: %f", multiply(testA, testB));
+    printf("\n Divide: %f", divide(testB, testA));
     return 0;
 }
