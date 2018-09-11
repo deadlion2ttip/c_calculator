3b18f7e0188b6e6e0863e9c2849cf43b9b51ff33
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-09-11 16:36:11 -0400

    Calculator loops at end. All 4 functions included.

--- c_calculator/main.c
+++ c_calculator/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 
 double multiply(double A, double B){
 
@@ -20,44 +22,86 @@ double subtract(double A, double B){
 
 return A - B;
 }
+int isdigit(int c);
 
-void sanitizeOp();
+char sanitizeOp();
+
+double sanitizeNum();
+
+char sanitizeAgain();
 
 void main(){
     char operation;
     char again;
+    double A;
+    double B;
+
+    printf("Welcome to the Simple C calculator!\nWhat is your first number?: ");
+    A = sanitizeNum();
+
     printf("What operation would you like to perform? (type +, -, *, or /): ");
-    sanitizeOp();
+    operation = sanitizeOp();
+
+    printf("What is your second number?: ");
+    B = sanitizeNum();
 
 
+    if(operation == '+'){
+        printf("Add: %f\n", add(A, B));
+
+    } else if (operation == '-'){
+        printf("Subtract: %f\n", subtract(B, A));
+
+    } else if (operation == '*'){
+        printf("Multiply: %f\n", multiply(A, B));
+
+    } else if (operation == '/'){
+        printf("Divide: %f\n", divide(B, A));
+    }
     printf("Calculate something else? (y/n) ");
+    again = sanitizeAgain();
+
+    if (again == 'y'){
+        main();
+    } else{
+        printf("Goodbye!");
+    }
 
 }
 
-void sanitizeOp(){
-char operation;
-    double testA = 2;
-    double testB = 4;
-   scanf("%c", &operation);
+char sanitizeAgain(){
+char again;
+
+scanf("%s", &again);
+
+if (again == 'y' || again == 'n'){
+    return again;
+} else {
+    printf("Please type y or n\n");
+    return sanitizeAgain();
+}
+}
+
+char sanitizeOp(){
+    char operation;
+
+   scanf("%s", &operation);
 
     printf("operation: %c\n", operation);
 
-    if(operation == '+'){
-        printf("Add: %f\n", add(testA, testB));
-
-    } else if (operation == '-'){
-        printf("Subtract: %f\n", subtract(testB, testA));
-
-    } else if (operation == '*'){
-        printf("Multiply: %f\n", multiply(testA, testB));
-
-    } else if (operation == '/'){
-        printf("Divide: %f\n", divide(testB, testA));
-
-    } else if(operation == '\n'){
-
-    } else {
+    if(operation == '+' || operation == '-' || operation == '*' || operation == '/'){
+        return operation;
+    }  else {
         printf("Please use a valid operator(+, -, *, or /)");
-        sanitizeOp();
+        return sanitizeOp();
     }
+}
+
+double sanitizeNum(){
+    double number;
+    scanf("%lf", &number);
+    printf("\nnumber is %f\n", number);
+
+    return number;
+
 }
